b55654f3a7f025bb6c25a62c4fb26eb69c1b0bd7
Cosmitar <mhherrera31@gmail.com>
2016-01-24 21:35:21 -0300

    gh-pages update

---
+++ bin/gh-deploy.js
@@ -0,0 +1,18 @@
+#! /usr/bin/env node
+
+var exec = require('child_process').exec;
+function puts(error, stdout, stderr) {
+    if (stderr) {
+    console.error(stderr);
+    return;
+  }
+  console.log(stdout);
+}
+
+var gitCommands = [
+    'git add public/',
+    'git commit -m "gh-pages update"',
+    'git subtree push --prefix public origin gh-pages'
+].join(' && ');
+
+exec(gitCommands, puts);
