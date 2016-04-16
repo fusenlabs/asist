79bf585b7d21b5ecc262f3ba27baa3a3e26727c5
Joel <joelalovera@gmail.com>
2016-04-16 00:45:50 -0300

    better reducers

--- app/reducers/app.js
+++ app/reducers/app.js
@@ -2,14 +2,20 @@ import { stateTree } from './../store/state';
 
 const initialState = Object.assign({}, stateTree.counter);
 
+const fusenrux = (reducer, actionType) => {
+  return (reducer.hasOwnProperty(actionType) && reducer[actionType] || reducer.DEFAULT)();
+};
+
 export default (state = initialState, action) => {
-  const switchObj = {
-    INCREMENT: () => {
-      let newState = Object.assign({}, state);
-      newState.value++;
-      return newState;
-    },
-    DEFAULT: ()=> state
+  const reducer = {};
+
+  reducer.INCREMENT = () => {
+    let newState = Object.assign({}, state);
+    newState.value++;
+    return newState;
   };
-  return (switchObj.hasOwnProperty(action.type) && switchObj[action.type] || switchObj.DEFAULT)();
+
+  reducer.DEFAULT = () => state;
+
+  return fusenrux(reducer, action.type);
 };
