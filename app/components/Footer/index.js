7ddddb08860dc30a7c0b70c58320b8d4bc5d7938
Cosmitar <mhherrera31@gmail.com>
2017-03-09 11:20:26 -0300

    refactor directory structure

--- app/components/Footer/index.js
+++ app/components/Footer/index.js
@@ -5,10 +5,14 @@ const Footer = () => {
     <footer className="footer">
       <div className="container level">
         <div className="level-left animated fadeInUp">
-          made by FūsenLabs in Córdoba, Argentina
+          made by <a href="http://fusenlabs.com" target="_blank">FūsenLabs</a> in C&oacute;rdoba, Argentina
         </div>
         <div className="level-right animated fadeInUp">
-          GitHub
+          <a href="https://github.com/fusenlabs/asist" target="_blank">
+            <span className="icon">
+              <i className="fa fa-github"></i>
+            </span>
+          </a>
         </div>
       </div>
     </footer>
--- app/containers/Home/index.js
+++ app/containers/Home/index.js
@@ -1,41 +1,14 @@
 import React, { Component } from 'react';
-import ReactCSSTransitionGroup from 'react-addons-css-transition-group';
 import { connect } from 'react-redux';
 import { bindActionCreators } from 'redux';
 import * as AppActions from './../../actions/app';
 import { Modal } from './../../components/modal';
-import List from './../List';
-import Nav from './../Nav';
-import Footer from './../Footer';
-import Clock from './../Clock';
+import List from './../../components/List';
+import Nav from './../../components/Nav';
+import Footer from './../../components/Footer';
+import Clock from './../../components/Clock';
 import { ImageProvider } from './../../utils';
 import './_index.scss';
-
-const Welcome = () => {
-  return (
-    <div className="hero-body">
-      <div className="container has-text-centered">
-        <ReactCSSTransitionGroup
-          transitionName={{
-            enter: 'fadeInDown',
-            enterActive: 'fadeInDown',
-            leave: 'fadeOutUp',
-            leaveActive: 'fadeOutUp',
-            appear: 'fadeInDown',
-            appearActive: 'fadeInDown',
-          }}
-          transitionAppear={ true }
-          transitionAppearTimeout={ 1000 }
-          transitionEnterTimeout={ 1000 }
-          transitionLeaveTimeout={ 1 }
-        >
-          <Nav />
-        </ReactCSSTransitionGroup>
-        <List />
-      </div>
-    </div>
-  );
-};
 
 class HomeContainer extends Component {
   constructor(props) {
